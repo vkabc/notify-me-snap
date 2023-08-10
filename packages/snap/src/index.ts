25b9d1c3f84b41c956df2c124391538a140c4ace
koxuan <khongjiaxuan@gmail.com>
2023-08-10 09:54:55 +0800

    feat: notifications on cronjob

--- packages/snap/src/index.ts
+++ packages/snap/src/index.ts
@@ -1,6 +1,8 @@
 import { OnRpcRequestHandler } from '@metamask/snaps-types';
-import { panel, text } from '@metamask/snaps-ui';
+import { panel, text, heading } from '@metamask/snaps-ui';
 
+import { rpcErrors } from '@metamask/rpc-errors';
+import type { OnCronjobHandler } from '@metamask/snaps-types';
 /**
  * Handle incoming JSON-RPC requests, sent through `wallet_invokeSnap`.
  *
@@ -22,6 +24,39 @@ export const onRpcRequest: OnRpcRequestHandler = ({ origin, request }) => {
         },
 
       });
+    default:
+      throw new Error('Method not found.');
+  }
+};
+
+
+/**
+ * Handle cronjob execution requests from MetaMask. This handler handles one
+ * method:
+ *
+ * - `execute`: The JSON-RPC method that is called by MetaMask when the cronjob
+ * is triggered. This method is specified in the snap manifest under the
+ * `endowment:cronjob` permission. If you want to support more methods (e.g.,
+ * with different times), you can add them to the manifest there.
+ *
+ * @param params - The request parameters.
+ * @param params.request - The JSON-RPC request object.
+ * @returns The JSON-RPC response.
+ * @see https://docs.metamask.io/snaps/reference/exports/#oncronjob
+ */
+
+
+export const onCronjob: OnCronjobHandler = async ({ request }) => {
+  switch (request.method) {
+    case 'execute':
+      return snap.request({
+        method: 'snap_notify',
+        params: {
+          type: 'native',
+          message: `Hello, world!`,
+        },
+      });
+
     default:
       throw new Error('Method not found.');
   }
